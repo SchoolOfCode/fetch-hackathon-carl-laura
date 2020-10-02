f32779586fe25abaad4a9c8597f8df63ece4cd6f
Your name herecod3rcarl <your_email@example.comcod3rcarl@gmail.com>
2020-10-02 13:41:36 +0100

    working pokedex

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
+let number = 1
 let p1 = document.querySelector("#p1");
 let p2 = document.querySelector("#p2");
 let p3 = document.querySelector("#p3");
 let p4 = document.querySelector("#p4");
-
 let h1 = document.querySelector("h1");
-
-
 async function getPokemon() {
-    let response = await fetch("https://pokeapi.co/api/v2/pokemon/1/");
+
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}`);
+   
     let data = await response.json();
-    console.log(data.sprites.front_default);
     let pokemonName = data.name;
-    let pokemonNo = data.order;
+    let pokemonNo = data.id;
+    
     let pokemonHeight = data.height;
     let pokemonWeight= data.weight;
     let pokemonMove1 = data.moves[0].move.name;
     let pokemonMove2 = data.moves[1].move.name;
     let pokemonMove3 = data.moves[2].move.name;
     let pokemonMove4 = data.moves[3].move.name;
-    let pokemonMove5 = data.moves[4].move.name;
+    
     h1.innerText = `${pokemonName}, I CHOOSE YOU!`;
-    p1.innerText = `Pokemon Number: ${pokemonNo}`;
-    p2.innerText = `Pokemon Height: ${pokemonHeight}`;
-    p3.innerText = `Pokemon Weight: ${pokemonWeight}`;;
-    p4.innerText = `Pokemon Moves: ${pokemonMove1}, ${pokemonMove2}, ${pokemonMove3}, ${pokemonMove4}, ${pokemonMove5}`;
-    img = document.createElement("img");
+
+    p1.innerText = `Number: ${pokemonNo}`;
+    p2.innerText = `Height: ${pokemonHeight}`;
+    p3.innerText = `Weight: ${pokemonWeight}`;;
+    p4.innerText = `Moves: ${pokemonMove1}, ${pokemonMove2}, ${pokemonMove3}, ${pokemonMove4}`;
+    img = document.querySelector("img");
     img.src = data.sprites.front_default;
     body = document.querySelector("body");
     body.appendChild(img);
+    console.log(data)
 } getPokemon();
+
+let nextButton = document.querySelector("#next-btn")
+nextButton = document.addEventListener("click", counter)
+
+function counter() {
+    number++
+    return getPokemon()
+}
+
+let previousButton = document.querySelector("#previous-btn")
+previousButton = document.addEventListener("click", counterBack)
+
+function counterBack() {
+    number--
+    return getPokemon()
+}
